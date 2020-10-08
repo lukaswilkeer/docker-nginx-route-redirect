908c15b312026b52f3e6913e637177ad9349ea5e
Lukas W. Santos <lukasc4l@gmail.com>
2020-10-08 17:28:23 -0300

    fixme: port and environment variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const port = 3000
+const environment = 'production'
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
